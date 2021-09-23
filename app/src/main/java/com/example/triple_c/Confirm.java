ff77d44cd315c04bcada503d91be0de50fcee8d3
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-09-23 19:03:51 +0300

    add auth configure

--- app/src/main/java/com/example/triple_c/Confirm.java
+++ app/src/main/java/com/example/triple_c/Confirm.java
@@ -2,7 +2,13 @@ package com.example.triple_c;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.amplifyframework.core.Amplify;
 
 public class Confirm extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class Confirm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+
+        EditText confirmationInput = findViewById(R.id.confirmationInput);
+        EditText usernameConfirm = findViewById(R.id.usernameConfirm);
+        Button confirmButton = findViewById(R.id.confirmButton);
+
+        confirmButton.setOnClickListener(v -> {
+            Amplify.Auth.confirmSignUp(
+                    usernameConfirm.getText().toString(),
+                    confirmationInput.getText().toString(),
+                    result -> Log.i("AuthQuickstart", result.isSignUpComplete() ? "Confirm signUp succeeded" : "Confirm sign up not complete"),
+                    error -> Log.e("AuthQuickstart", error.toString())
+            );
+
+            Intent goHome = new Intent(Confirm.this, MainActivity.class);
+            startActivity(goHome);
+        });
     }
 }
--- app/src/main/java/com/example/triple_c/SignIn.java
+++ app/src/main/java/com/example/triple_c/SignIn.java
@@ -2,7 +2,13 @@ package com.example.triple_c;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.amplifyframework.core.Amplify;
 
 public class SignIn extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        EditText username = findViewById(R.id.usernameSignin);
+        EditText password = findViewById(R.id.passwordSignin);
+        Button signin = findViewById(R.id.signinButton);
+
+        signin.setOnClickListener(v-> Amplify.Auth.signIn(
+                username.getText().toString(),
+                password.getText().toString(),
+                result -> {
+                    Log.i("AuthQuickstart", result.isSignInComplete() ? "Sign in succeeded" : "Sign in not complete");
+                    startActivity(new Intent(SignIn.this, MainActivity.class));
+                },
+                error -> Log.e("AuthQuickstart", error.toString())
+        ));
+
     }
 }
