6193a818ca7bf1aca9344085a3495058671de4e8
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-09-27 13:01:04 +0300

    signup-signin

--- app/src/main/java/com/example/triple_c/MainActivity.java
+++ app/src/main/java/com/example/triple_c/MainActivity.java
@@ -29,63 +29,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            Amplify.addPlugin(new AWSApiPlugin());
-            Amplify.addPlugin(new AWSCognitoAuthPlugin());
-            Amplify.addPlugin(new AWSS3StoragePlugin());
-
-            Amplify.configure(getApplicationContext());
-
-
-            Log.i("MyAmplifyApp", "Initialized Amplify");
-        } catch (AmplifyException error) {
-            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
-        }
-
         AuthUser authUser = Amplify.Auth.getCurrentUser();
-        if (authUser != null) {
-            String username = authUser.getUsername();
-            Amplify.API.query(
-                    ModelQuery.list(com.amplifyframework.datastore.generated.model.User.class),
-                    response -> {
-                        for (User user : response.getData()) {
-                            if (user.getUsername().equals(username)) {
-                                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-                                SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
-                                sharedPreferencesEditor.putString("userId", user.getId());
-                                sharedPreferencesEditor.putString("username", user.getUsername());
-                                sharedPreferencesEditor.apply();
-                            }
+        String username = authUser.getUsername();
+        Amplify.API.query(
+                ModelQuery.list(com.amplifyframework.datastore.generated.model.User.class),
+                response -> {
+                    for (User user : response.getData()) {
+                        if (user.getUsername().equals(username)) {
+                            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                            SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+                            sharedPreferencesEditor.putString("userId", user.getId());
+                            sharedPreferencesEditor.putString("username", user.getUsername());
+                            sharedPreferencesEditor.apply();
                         }
-                    },
-                    error -> Log.e("MyAmplifyApp", error.toString(), error)
-            );
-        }
+                    }
+                },
+                error -> Log.e("MyAmplifyApp", error.toString(), error)
+        );
+
         Button signoutButton = findViewById(R.id.signoutButton);
         signoutButton.setOnClickListener(v -> {
             Amplify.Auth.signOut(
-                    () -> Log.i("AuthQuickstart", "Signed out successfully"),
+                    () -> {
+                        Log.i("AuthQuickstart", "Signed out successfully");
+                        Intent goToSignIn = new Intent(MainActivity.this, SignIn.class);
+                        startActivity(goToSignIn);
+                        finish();
+                    },
                     error -> Log.e("AuthQuickstart", error.toString())
             );
         });
 
 
-        RelativeLayout serviscesMain =findViewById(R.id.servicesmain);
-        RelativeLayout profilemain =findViewById(R.id.profilemain);
-        RelativeLayout contactusmain =findViewById(R.id.contactusmain);
+        RelativeLayout serviscesMain = findViewById(R.id.servicesmain);
+        RelativeLayout profilemain = findViewById(R.id.profilemain);
+        RelativeLayout contactusmain = findViewById(R.id.contactusmain);
 
         serviscesMain.setOnClickListener(new View.OnClickListener() {
-                                             @Override
-                                             public void onClick(View view) {
-                                                 Intent goToOurServices = new Intent(MainActivity.this, OurServices.class);
-                                                 startActivity(goToOurServices);
-                                             }
-                                         });
+            @Override
+            public void onClick(View view) {
+                Intent goToOurServices = new Intent(MainActivity.this, OurServices.class);
+                startActivity(goToOurServices);
+            }
+        });
 
         profilemain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent goToProfileMain=new Intent(MainActivity.this,Profile.class);
+                Intent goToProfileMain = new Intent(MainActivity.this, Profile.class);
                 startActivity(goToProfileMain);
             }
         });
@@ -93,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         contactusmain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent goToContactUsMains=new Intent(MainActivity.this,ContactUs.class);
+                Intent goToContactUsMains = new Intent(MainActivity.this, ContactUs.class);
                 startActivity(goToContactUsMains);
             }
         });
