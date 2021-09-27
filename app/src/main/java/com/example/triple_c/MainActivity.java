8880e254da411393fce1a41d07c9019d321945b2
Mohammadabufarweh <abufarweh34@gmail.com>
2021-09-27 11:02:27 +0300

    mainactivity

--- app/src/main/java/com/example/triple_c/MainActivity.java
+++ app/src/main/java/com/example/triple_c/MainActivity.java
@@ -7,8 +7,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
@@ -81,51 +83,78 @@ public class MainActivity extends AppCompatActivity {
             );
         });
 
-        Button addCar = findViewById(R.id.addCarButton);
-//        Button askForHelp = findViewById(R.id.askForServices);
-        Button confirm = findViewById(R.id.confirmPagee);
-        Button contactUs = findViewById(R.id.contactUs);
-        Button details = findViewById(R.id.details);
-        Button ourServicesPage = findViewById(R.id.ourServicesPage);
-        Button profile = findViewById(R.id.profile);
-        Button signIn = findViewById(R.id.signIn);
-        Button signUp = findViewById(R.id.signUp);
-        Button splash = findViewById(R.id.splash);
-        Button Details = findViewById(R.id.Details);
+        RelativeLayout serviscesMain =findViewById(R.id.servicesmain);
+        RelativeLayout profilemain =findViewById(R.id.profilemain);
+        RelativeLayout contactusmain =findViewById(R.id.contactusmain);
 
-        addCar.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, AddCar.class));
+        serviscesMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToOurServices=new Intent(MainActivity.this,OurServices.class);
+                startActivity(goToOurServices);
+            }
         });
-//        askForHelp.setOnClickListener(view -> {
-//            startActivity(new Intent(MainActivity.this, AskForService.class));
+
+        profilemain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToProfileMain=new Intent(MainActivity.this,Profile.class);
+                startActivity(goToProfileMain);
+            }
+        });
+        contactusmain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToContactUsMains=new Intent(MainActivity.this,ContactUs.class);
+                startActivity(goToContactUsMains);
+            }
+        });
+
+//        Button addCar = findViewById(R.id.addCarButton);
+////        Button askForHelp = findViewById(R.id.askForServices);
+//        Button confirm = findViewById(R.id.confirmPagee);
+//        Button contactUs = findViewById(R.id.contactUs);
+//        Button details = findViewById(R.id.details);
+//        Button ourServicesPage = findViewById(R.id.ourServicesPage);
+//        Button profile = findViewById(R.id.profile);
+//        Button signIn = findViewById(R.id.signIn);
+//        Button signUp = findViewById(R.id.signUp);
+//        Button splash = findViewById(R.id.splash);
+//        Button Details = findViewById(R.id.Details);
+
+//        addCar.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, AddCar.class));
 //        });
-        confirm.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, Confirm.class));
-        });
-        contactUs.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, ContactUs.class));
-        });
-        details.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, Details.class));
-        });
-        ourServicesPage.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, OurServices.class));
-        });
-        profile.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, Profile.class));
-        });
-        signIn.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, SignIn.class));
-        });
-        signUp.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, SignUp.class));
-        });
-        splash.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, Splash.class));
-        });
-        Details.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, Details.class));
-        });
+////        askForHelp.setOnClickListener(view -> {
+////            startActivity(new Intent(MainActivity.this, AskForService.class));
+////        });
+//        confirm.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, Confirm.class));
+//        });
+//        contactUs.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, ContactUs.class));
+//        });
+//        details.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, Details.class));
+//        });
+//        ourServicesPage.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, OurServices.class));
+//        });
+//        profile.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, Profile.class));
+//        });
+//        signIn.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, SignIn.class));
+//        });
+//        signUp.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, SignUp.class));
+//        });
+//        splash.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, Splash.class));
+//        });
+//        Details.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, Details.class));
+//        });
 
     }
 }
